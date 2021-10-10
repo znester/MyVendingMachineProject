afd2720401bbea54cf421c275d97f23366132bae
Zachary Nester <znester52@gmail.com>
2021-10-09 20:23:40 -0400

    Made a couple tests

--- java/src/test/java/com/techelevator/view/CoinBoxTests.java
+++ java/src/test/java/com/techelevator/view/CoinBoxTests.java
@@ -8,14 +8,19 @@ public class CoinBoxTests {
     CoinBox testBox = new CoinBox();
 
     @Test
-    public void _01_Feeds_1000_Deposited_Equals_1000(){
-
+    public void _01_feed_1000_Deposited_Equals_1000(){
         testBox.feed(1000);
-
         int actualAmount = testBox.getMoneyDeposited();
-
         Assert.assertEquals(1000,actualAmount);
-
     }
+
+    @Test
+    public void _02_spend_1000_deposited_removes_1000() {
+        testBox.feed(1100);
+        testBox.spend(1000);
+        int actualAmount = testBox.getMoneyDeposited();
+        Assert.assertEquals(100, actualAmount);
+    }
+
 
 }
